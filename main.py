ca14da08fd722a0a51fa676e37f30c65d9886ed2
hussein <021hossein@gmail.com>
2023-06-04 15:44:34 +0330

    make download() async

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ client_id = '5f2fb8894e464653bd0c2fbd81e957a6'
 client_secret = '789ef1750b48450ab767e7b44eb6f2c7'
 bot_token = '5927525345:AAFNHk_jxBQXof3UEW2I--3zG71485Dnh1E'
 chat_id = '@EclecticEuphoria'
-interval = 10
+interval = 15
 
 # Initialize the Spotify API client
 client_credentials_manager = SpotifyClientCredentials(client_id=client_id, client_secret=client_secret)
@@ -38,7 +38,7 @@ def check_recently_added_tracks(playlist_uri):
         added_at = track['added_at']
         artist_name = track['track']['artists'][0]['name']
         url = track['track']['external_urls']['spotify']
-        filename = f"{artist_name} - {track_name}.mp3"
+        path = f"./{artist_name} - {track_name}.mp3"
 
         # Convert the added_at timestamp to a datetime object
         added_time = datetime.datetime.strptime(added_at, "%Y-%m-%dT%H:%M:%SZ")
@@ -48,23 +48,23 @@ def check_recently_added_tracks(playlist_uri):
         # Check if the track was added within the last 10 seconds
         if time_difference.total_seconds() <= interval:
             print(f"Recently added track: {track_name}")
-            download(url, filename)
-            send_and_remove_music(f"./{filename}")
+            download_and_send(url, path)
 
     time.sleep(interval)
     check_recently_added_tracks(playlist_uri)
 
 
-def send_and_remove_music(path):
+async def download_and_send(url, path):
+    command = f"python -m spotdl {url}"
+    print(f"download is starting: {path}")
+    process = await asyncio.create_subprocess_shell(command)
+    await process.communicate()
+    print(f"sending: {path}")
     asyncio.run(send_music(bot_token, chat_id, path))
     # remove file after sending
+    print(f"removing: {path}")
     os.remove(path)
-
-
-def download(url, filename):
-    command = f"python -m spotdl {url}"
-    subprocess.run(command, shell=True)
-    return filename
+    return path
 
 
 if __name__ == '__main__':
